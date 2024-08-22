16818a56580d6de50c1ceb20de3aca6ca3f1baf8
Joel <114038166+joelcordoba@users.noreply.github.com>
2024-08-22 11:06:01 -0500

    regist_serviceWorker.js

--- regist_serviceWorker.js
+++ regist_serviceWorker.js
@@ -1,15 +1,9 @@
+// Verifica si el navegador soporta Service Workers
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./Service_Worker.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
-}
-
-/* tambien se puede registrar el servi WORKER de esta manera
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("./Service_Worker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+  // Espera a que la página cargue completamente antes de registrar el Service Worker
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./Service_Worker.js')
+      .then(reg => console.log('Registro de Service Worker exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el Service Worker', err));
+  });
 }
