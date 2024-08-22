e4147879b46922605d8cb75c7ca9a7a245dd777c
Joel <114038166+joelcordoba@users.noreply.github.com>
2024-08-22 09:34:31 -0500

    Service_Worker.js

--- Service_Worker.js
+++ Service_Worker.js
@@ -1,54 +1,64 @@
-// Nombre de la cache
-const CACHE_NAME = 'chatiapues-cache-v1';
-
-// Archivos a cachear
-const urlsToCache = [
-    '/',
+// Asignar un nombre y versión al cache
+const CACHE_NAME = 'Chatiapues-v1',
+  urlsToCache = [
+    '/', // Si estás trabajando en la raíz, esto debería funcionar; si no, ajusta la ruta.
     '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    'https://cdn.jsdelivr.net/gh/joelcordoba/Chatiapues/manifest.json'
-];
+    '/css/bootstrap.min.css',
+    '/css/home.css',
 
-// Instalando el Service Worker y cacheando los recursos
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+    '/js/bootstrap.bundle.min.js',
+    '/js/jquery-3.6.0.js',
+
+    '/regist_serviceWorker.js',
+    '/pwa/images/icons/icon-512x512.png',
+    '/pwa/images/icons/icon-72x72.png'
+  ];
+
+// Durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting());
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  );
 });
 
-// Activando el Service Worker y limpiando caches antiguas
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+// Una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            // Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  );
 });
 
-// Interceptando las solicitudes y sirviendo desde la cache
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Si el recurso está en la cache, lo devuelve
-                if (response) {
-                    return response;
-                }
-                // Si no está en la cache, lo solicita a la red
-                return fetch(event.request);
-            })
-    );
+// Cuando el navegador recupera una URL
+self.addEventListener('fetch', e => {
+  // Responder ya sea con el objeto en caché o continuar y buscar la URL real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          // Recuperar del cache
+          return res;
+        }
+        // Recuperar de la petición a la URL
+        return fetch(e.request);
+      })
+  );
 });
